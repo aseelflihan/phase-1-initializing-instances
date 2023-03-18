cfa9e70e7aae4e6a806d6d0736150ab0988fbd24
aseelflihan <aseelflihan@gmail.com>
2023-03-18 16:06:36 +0300

    complate assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code here
+class Dinner {
+    constructor(salad, soup, entree) {
+      this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
+    }
+  }
+  class Lunch {
+    constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+  }
+  class Breakfast {
+    constructor(food, drink) {
+      this.food = food;
+      this.drink = drink;
+    }
+  }
